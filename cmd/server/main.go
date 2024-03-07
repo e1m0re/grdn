9bcb499891cd209e25c67ccc54f536ac42d04dc1
vlogashenko <vl@phishman.ru>
2024-03-07 17:17:32 +0300

    Increment 1: Init server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,76 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type MemStorage struct {
+	data map[string]int
+}
+
+func (store *MemStorage) UpdateMetric(mType string, name string, value string) {
+	i, _ := strconv.Atoi(value)
+	switch mType {
+	case "gauge", "float64":
+		store.data[name] = i
+	case "counter", "int64":
+		store.data[name] += i
+	}
+}
+
+var storage = MemStorage{data: map[string]int{}}
+
+func isValidMetricsType(value string) bool {
+	switch value {
+	case "gauge", "float64", "counter", "int64":
+		return true
+	default:
+		return false
+	}
+}
+
+func isValidValue(value string) bool {
+	_, err := strconv.Atoi(value)
+	return err == nil
+}
+
+func updateMetricHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		http.Error(response, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// check type
+	pathParams := strings.Split(request.RequestURI, "/")
+	if len(pathParams) < 3 || !isValidMetricsType(pathParams[2]) {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// check name
+	if len(pathParams) < 4 || len(pathParams[3]) == 0 {
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// check value
+	if len(pathParams) < 5 || !isValidValue(pathParams[4]) {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	storage.UpdateMetric(pathParams[2], pathParams[3], pathParams[4])
+
+	return
+}
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/`, updateMetricHandler)
+
+	err := http.ListenAndServe(`localhost:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/e1m0re/grdn
+
+go 1.21.8
